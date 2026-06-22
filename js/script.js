18084dab869caeee845c887972e9f3ff799b471c
Martino Pagliarani <martipaglia02@gmail.com>
2026-06-22 14:33:14 +0200

    Enhance project showcase with carousel functionality and responsive design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Email copy to clipboard functionality
+// Snackbar message display
+
 var snackbarHideTimeoutId = null;
 
 function copyToClipboard(text) {
@@ -19,4 +22,28 @@ function showEmailCopiedMessage() {
     snackbarHideTimeoutId = window.setTimeout(function () {
         snackbar.classList.remove("show");
     }, 2000);
+}
+
+// Carousel functionality
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+
 }
